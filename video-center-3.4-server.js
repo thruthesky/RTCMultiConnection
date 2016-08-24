32f4dc5ac6ca8f4dc772369c09737459c59453a6
jeraisy21 <thruthesky@gmail.com>
2016-08-24 22:48:05 +0900

    add more protocol

--- video-center-3.4-server.js
+++ video-center-3.4-server.js
@@ -1,15 +1,46 @@
+var lobbyRoomName = 'Lobby';
 var VideoCenterServer = (function () {
     function VideoCenterServer() {
+        this.Users = new Array();
         console.log("VideoCenterServer::constructor() ...");
     }
     VideoCenterServer.prototype.listen = function (socket, io) {
+        var _this = this;
         console.log('VideoCenterServer::listen()');
-        this.socket = socket;
         this.io = io;
-        socket.on('ping', function (callback) {
-            console.log("I got ping. pong it.");
-            callback('pong');
+        this.addUser(socket);
+        socket.on('ping', this.pong);
+        socket.on('update-username', function (username, callback) {
+            _this.updateUsername(socket, username, callback);
         });
+    };
+    VideoCenterServer.prototype.addUser = function (socket) {
+        var user = {};
+        user.name = 'Anonymous';
+        user.room = lobbyRoomName;
+        user.socket = socket.id;
+        this.Users[socket.id] = user;
+        return this.Users[socket.id];
+    };
+    VideoCenterServer.prototype.setUser = function (user) {
+        this.Users[user.socket] = user;
+        return this.Users[user.socket];
+    };
+    VideoCenterServer.prototype.getUser = function (socket) {
+        return this.Users[socket.id];
+    };
+    VideoCenterServer.prototype.setUsername = function (socket, username) {
+        var user = this.getUser(socket);
+        user.name = username;
+        return this.setUser(user);
+    };
+    VideoCenterServer.prototype.updateUsername = function (socket, username, callback) {
+        var user = this.setUsername(socket, username);
+        callback(user);
+    };
+    VideoCenterServer.prototype.pong = function (callback) {
+        console.log("I got ping. pong it.");
+        callback('pong');
     };
     return VideoCenterServer;
 }());
--- video-center-3.4-server.ts
+++ video-center-3.4-server.ts
@@ -2,22 +2,70 @@
 /// <reference path="typings/globals/socket.io/index.d.ts" />
 /// import fs = require('fs');
 /// import oo = require('socket.io');
+
+interface User {
+    name: string;
+    room: string;
+    socket: string; // socket id
+}
+
+
+const lobbyRoomName = 'Lobby';
+
+
 class VideoCenterServer {
-    private socket: any;
     private io: any;
+    Users: Array<User> = new Array();
     constructor() {
         console.log("VideoCenterServer::constructor() ...");
     }
     listen(socket, io) {
         console.log('VideoCenterServer::listen()');
-        this.socket = socket;
         this.io = io;
+        this.addUser( socket );
+        socket.on('ping', this.pong )
+        socket.on('update-username', ( username, callback ) => {
+            this.updateUsername( socket, username, callback );
+         } );
+    }
+    
+    addUser ( socket: any ) : User {
+        let user: User = <User>{};
+        user.name = 'Anonymous';
+        user.room = lobbyRoomName;
+        user.socket = socket.id;
+        this.Users[ socket.id ] = user;
+        return this.Users[ socket.id ];
+    }
+    setUser ( user: User ) : User {
+        this.Users[ user.socket ] = user;
+        return this.Users[ user.socket ];
+    }
 
+    /**
+     * @code
+     *      var user = this.getUser( socket );
+     * @endcode
+     * 
+     */
+    getUser ( socket: any ) : User {
+        return this.Users[ socket.id ]
+    }
+    setUsername ( socket: any, username: string ) : User {
+        let user = this.getUser( socket );
+        user.name = username;
+        return this.setUser( user );
+    }
 
-        socket.on('ping', function( callback ) {
-            console.log("I got ping. pong it.");
-            callback('pong');
-        });
+    updateUsername ( socket: any, username: string, callback: any ) : void {
+        let user = this.setUsername( socket, username );
+        callback( user );
+        // vc.io.sockets.emit('update-username', user );
+    }
+
+    pong ( callback ) {
+        console.log("I got ping. pong it.");
+        callback('pong');
     }
 }
 exports = module.exports = VideoCenterServer;
