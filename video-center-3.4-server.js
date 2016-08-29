116a8f7aa21a7bae3651acaf8b39ef05eff333d6
ManuelManansala <manuelmanansalajr@gmail.com>
2016-08-29 15:10:09 +0800

    lobby chat

--- video-center-3.4-server.js
+++ video-center-3.4-server.js
@@ -12,8 +12,17 @@ var VideoCenterServer = (function () {
         socket.on('disconnect', function () {
             _this.disconnect(socket);
         });
+        socket.on('join-lobby', function (callback) {
+            _this.joinLobby(socket, callback);
+        });
         socket.on('update-username', function (username, callback) {
             _this.updateUsername(socket, username, callback);
+        });
+        socket.on('send-message', function (message, callback) {
+            _this.sendMessage(io, socket, message, callback);
+        });
+        socket.on('log-out', function (callback) {
+            _this.logout(socket, callback);
         });
     };
     VideoCenterServer.prototype.pong = function (callback) {
@@ -23,6 +32,13 @@ var VideoCenterServer = (function () {
     VideoCenterServer.prototype.disconnect = function (socket) {
         this.removeUser(socket.id);
         console.log("Someone Disconnected.");
+    };
+    VideoCenterServer.prototype.logout = function (socket, callback) {
+        var user = this.getUser(socket);
+        socket.leave(user.room);
+        this.removeUser(socket);
+        console.log(user.name + ' has logged out.');
+        callback();
     };
     VideoCenterServer.prototype.addUser = function (socket) {
         var user = {};
@@ -45,11 +61,23 @@ var VideoCenterServer = (function () {
         return this.setUser(user);
     };
     VideoCenterServer.prototype.updateUsername = function (socket, username, callback) {
+        var user_info = this.getUser(socket);
+        var oldusername = user_info.name;
         var user = this.setUsername(socket, username);
+        console.log(oldusername + " change it's name to " + username);
         callback(username);
+    };
+    VideoCenterServer.prototype.sendMessage = function (io, socket, message, callback) {
+        var user = this.getUser(socket);
+        io.sockets["in"](user.room).emit('get-message', { message: message, name: user.name, room: user.room });
+        callback(message);
     };
     VideoCenterServer.prototype.removeUser = function (id) {
         delete this.Users[id];
+    };
+    VideoCenterServer.prototype.joinLobby = function (socket, callback) {
+        socket.join(lobbyRoomName);
+        callback();
     };
     return VideoCenterServer;
 }());
--- video-center-3.4-server.ts
+++ video-center-3.4-server.ts
@@ -29,9 +29,21 @@ class VideoCenterServer {
         socket.on('disconnect', () => {
             this.disconnect( socket );
         });
+        socket.on('join-lobby', ( callback ) => {
+            this.joinLobby( socket, callback );
+            
+        } );
         socket.on('update-username', ( username, callback ) => {
             this.updateUsername( socket, username, callback );
-         } );
+            
+        } );
+        socket.on('send-message', ( message, callback ) => {
+            this.sendMessage( io, socket, message, callback );
+            
+        } );
+        socket.on('log-out', ( callback ) => {
+            this.logout( socket, callback );
+        } ); 
     }
     pong ( callback ) {
         console.log("I got ping. pong it.");
@@ -42,12 +54,20 @@ class VideoCenterServer {
         console.log("Someone Disconnected.");   
         // vc.io.sockets.emit('disconnect', socket.id);      
     }
+    logout ( socket: any, callback: any ) : void {
+        var user = this.getUser( socket );        
+        socket.leave( user.room );     
+        // vc.io.sockets.emit('log-out', socket);
+        this.removeUser( socket );
+        console.log(user.name + ' has logged out.' );              
+        callback();
+    }
     addUser ( socket: any ) : User {
         let user: User = <User>{};
         user.name = 'Anonymous';
         user.room = lobbyRoomName;
         user.socket = socket.id;
-        this.Users[ socket.id ] = user;
+        this.Users[ socket.id ] = user;         
         return this.Users[ socket.id ];
     }
     setUser ( user: User ) : User {
@@ -65,13 +85,26 @@ class VideoCenterServer {
     }
 
     updateUsername ( socket: any, username: string, callback: any ) : void {
-        let user = this.setUsername( socket, username );        
+        let user_info = this.getUser( socket );  
+        let oldusername = user_info.name;
+        let user = this.setUsername( socket, username );            
+        console.log(oldusername + " change it's name to "+username);    
         callback( username );
         // vc.io.sockets.emit('update-username', user );
     }
+    sendMessage ( io:any, socket: any, message: string, callback: any ) : void {
+        let user = this.getUser( socket );        
+        io.sockets["in"]( user.room ).emit('get-message', { message: message, name: user.name, room: user.room } );  
+        callback( message );
+      
+    }
+    
     removeUser ( id: string ) : void {
         delete this.Users[ id ]
     }  
-   
+    joinLobby ( socket: any,  callback: any ) : void {        
+        socket.join( lobbyRoomName ); 
+        callback();
+    }
 }
 exports = module.exports = VideoCenterServer;
